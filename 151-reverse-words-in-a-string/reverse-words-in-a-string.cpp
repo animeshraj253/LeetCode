fe4cdc8b4b4aa2920ca47468a9e2a45f3d52d710
Animesh Raj <134291311+animeshraj253@users.noreply.github.com>
2026-06-04 17:15:41 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (41.65%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -5,34 +5,23 @@ public:
         vector <string> ans;
         string word;
         
-        for( int i =s.size()-1; i>=0 ; i--){
-            if( s[i] == ' ' && word.size() > 0){
-                reverse(word.begin(), word.end());
+        for( int i =0 ; i<s.size(); i++){
+            if( s[i] != ' '){
+                word+=s[i];
+            }else if( !word.empty()){
                 ans.push_back(word);
-                word = "";
+                word="";
             }
-            else if ( s[i] != ' ') {
-                word.push_back(s[i]);
-
-            }    
         }
-        if ( word !=""){
-            reverse(word.begin(), word.end());
+        if ( word != ""){
             ans.push_back(word);
         }
 
 
-        // for( string s : ans){
-        //     cout<<s ;
-        // }cout<<"|";
-
-
         word = "";
-        // cout<< "size  = " << ans.size();
-        for(int i = 0 ; i< ans.size(); i++){
-            // cout<<word<<endl;
+        for( int i = ans.size() -1  ; i>=0; i--){
             word+= ans[i];
-            if ( i != ans.size()- 1 ){
+            if ( i != 0 ){
                 word += " ";
             }
         }
